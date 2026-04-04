46cca42204eeab1f5310335ad4f1f7241607ac5e
Dmitry Moiseenko <dmitrymoiseenko98@gmail.com>
2026-04-04 19:58:54 +0300

    Add any type to fix template type error

--- app/shopify.server.ts
+++ app/shopify.server.ts
@@ -10,7 +10,7 @@ const shopify = shopifyApp({
   scopes: process.env.SCOPES?.split(","),
   appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
-  sessionStorage: new PrismaSessionStorage(prisma),
+  sessionStorage: new PrismaSessionStorage(prisma) as any, // Template type error
   distribution: AppDistribution.AppStore,
   future: {
     expiringOfflineAccessTokens: true,
